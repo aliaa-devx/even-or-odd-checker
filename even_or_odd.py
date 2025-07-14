3aadca7f64622f544546d1d6c65e553cd53d1753
aliaay <aliaa.ashraf.dev@gmail.com>
2025-07-15 01:31:57 +0300

    Add even or odd checker project

---
+++ even_or_odd.py
@@ -0,0 +1,10 @@
+
+# Even or Odd Checker
+# Made by Alia using Python
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
